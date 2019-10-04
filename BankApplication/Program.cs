21b3e88011b766ebb75de8bc461507f122e63a42
Srisneha <srisnehakrishnaraj@gmail.com>
2019-10-04 13:37:18 -0700

    changing the Program class

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -6,27 +6,7 @@ namespace BankApplication
     {
         static void Main(string[] args)
         {
-            //Instance of an acount  == object
-
-            var account = Bank.CreateAccount("test@test.com", TypeOfAccounts.Checking, 200);
-
-
-            Console.WriteLine($"AN : {account.AccountNumber}," +
-                $"Balance : {account.Balance:c}," +
-                $"Created Date : {account.CreatedDate}," +
-                $"Created Date : {account.AccountType}"
-
-                );
-
-            var account2 = Bank.CreateAccount("test2@test.com", TypeOfAccounts.Savings, 300);
-
-
-            Console.WriteLine($"AN : {account2.AccountNumber}," +
-     $"Balance : {account2.Balance}," +
-     $"Created Date : {account2.CreatedDate}," +
-              $"Created Date : {account2.AccountType}");
-
-            Console.WriteLine("Welcome to my bank");
+            
 
             while (true)
             {
